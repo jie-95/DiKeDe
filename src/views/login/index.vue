1c37b177bb62713d632bc7b6a474dc0840e66ebf
yang <young_yang95@163.com>
2022-08-16 15:18:02 +0800

    登录样式调整

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,14 +1,20 @@
 <template>
   <div class="login-container">
-    <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" auto-complete="on" label-position="left">
-
+    <el-form
+      ref="loginForm"
+      :model="loginForm"
+      :rules="loginRules"
+      class="login-form"
+      auto-complete="on"
+      label-position="left"
+    >
       <div class="title-container">
         <h3 class="title">Login Form</h3>
       </div>
 
       <el-form-item prop="username">
         <span class="svg-container">
-          <svg-icon icon-class="user" />
+          <i class="el-icon-mobile-phone" />
         </span>
         <el-input
           ref="username"
@@ -23,7 +29,7 @@
 
       <el-form-item prop="password">
         <span class="svg-container">
-          <svg-icon icon-class="password" />
+          <i class="el-icon-lock" />
         </span>
         <el-input
           :key="passwordType"
@@ -37,17 +43,38 @@
           @keyup.enter.native="handleLogin"
         />
         <span class="show-pwd" @click="showPwd">
-          <svg-icon :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'" />
+          <svg-icon
+            :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'"
+          />
         </span>
       </el-form-item>
+      <!-- 验证码 -->
+      <el-form-item>
+        <span class="keyImg">
+          <span class="svg-container">
+            <i class="el-icon-picture-outline" />
+          </span>
+          <el-input
+            placeholder="请输入验证码"
+            tabindex="2"
+            auto-complete="on"
+          />
+          <img src="../../assets/imgs/test1.jpg" alt="" @click="changrImg">
+        </span>
+      </el-form-item>
+      <!-- 验证码 -->
 
-      <el-button :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="handleLogin">Login</el-button>
+      <el-button
+        :loading="loading"
+        type="primary"
+        style="width: 100%; margin-bottom: 30px"
+        @click.native.prevent="handleLogin"
+      >Login</el-button>
 
       <div class="tips">
-        <span style="margin-right:20px;">username: admin</span>
+        <span style="margin-right: 20px">username: admin</span>
         <span> password: any</span>
       </div>
-
     </el-form>
   </div>
 </template>
@@ -78,8 +105,12 @@ export default {
         password: '111111'
       },
       loginRules: {
-        username: [{ required: true, trigger: 'blur', validator: validateUsername }],
-        password: [{ required: true, trigger: 'blur', validator: validatePassword }]
+        username: [
+          { required: true, trigger: 'blur', validator: validateUsername }
+        ],
+        password: [
+          { required: true, trigger: 'blur', validator: validatePassword }
+        ]
       },
       loading: false,
       passwordType: 'password',
@@ -106,20 +137,26 @@ export default {
       })
     },
     handleLogin() {
-      this.$refs.loginForm.validate(valid => {
+      this.$refs.loginForm.validate((valid) => {
         if (valid) {
           this.loading = true
-          this.$store.dispatch('user/login', this.loginForm).then(() => {
-            this.$router.push({ path: this.redirect || '/' })
-            this.loading = false
-          }).catch(() => {
-            this.loading = false
-          })
+          this.$store
+            .dispatch('user/login', this.loginForm)
+            .then(() => {
+              this.$router.push({ path: this.redirect || '/' })
+              this.loading = false
+            })
+            .catch(() => {
+              this.loading = false
+            })
         } else {
           console.log('error submit!!')
           return false
         }
       })
+    },
+    changrImg() {
+      console.log(1233)
     }
   }
 }
@@ -129,8 +166,8 @@ export default {
 /* 修复input 背景不协调 和光标变色 */
 /* Detail see https://github.com/PanJiaChen/vue-element-admin/pull/927 */
 
-$bg:#283443;
-$light_gray:#fff;
+$bg: #283443;
+$light_gray: #fff;
 $cursor: #fff;
 
 @supports (-webkit-mask: none) and (not (cater-color: $cursor)) {
@@ -152,7 +189,7 @@ $cursor: #fff;
       -webkit-appearance: none;
       border-radius: 0px;
       padding: 12px 5px 12px 15px;
-      color: $light_gray;
+      color: #999;
       height: 47px;
       caret-color: $cursor;
 
@@ -165,31 +202,46 @@ $cursor: #fff;
 
   .el-form-item {
     border: 1px solid rgba(255, 255, 255, 0.1);
-    background: rgba(0, 0, 0, 0.1);
+    background: #fff;
     border-radius: 5px;
     color: #454545;
+    border: 1px solid #e2e2e2;
+    border-radius: 4px;
   }
 }
 </style>
 
 <style lang="scss" scoped>
-$bg:#2d3a4b;
-$dark_gray:#889aa4;
-$light_gray:#eee;
+$bg: #2d3a4b;
+$dark_gray: #889aa4;
+$light_gray: #eee;
 
 .login-container {
+  position: relative;
   min-height: 100%;
   width: 100%;
+  background: url('../../assets/imgs/background-login.png');
+  background-repeat: no-repeat;
+  background-size: cover;
   background-color: $bg;
   overflow: hidden;
 
   .login-form {
-    position: relative;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    margin-top: -194px;
+    margin-left: -259px;
     width: 520px;
     max-width: 100%;
-    padding: 160px 35px 0;
-    margin: 0 auto;
     overflow: hidden;
+    background-color: #fff;
+    border-radius: 10px;
+    padding: 76px 35px 0;
+    box-shadow: 0 3px 70px 0 rgb(30 111 72 / 35%);
+  }
+  .keyImg {
+    display: flex;
   }
 
   .tips {
